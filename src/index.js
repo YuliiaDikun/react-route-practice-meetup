eb7f4797a45a4b32b1c0563ec0c4fe05f5eab0a3
Yuliia Dikun <107618913+YuliiaDikun@users.noreply.github.com>
2023-01-26 18:48:48 +0200

    CI: package.json

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <FavoritesContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-route-practice-meetup/">
         <App />
       </BrowserRouter>
     </FavoritesContextProvider>
