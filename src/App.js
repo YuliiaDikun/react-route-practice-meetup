b1e79d5343d65e007d0898f88c79fb0ffb5cb1c2
Yuliia Dikun <dikun.yulia@gmail.com>
2023-02-21 23:14:25 +0200

    feat: error page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetup";
 import NewMeetup from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
+import Error from "./pages/Error";
 import Layout from "./components/Layout/Layout";
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<AllMeetups />} />
         <Route path="/newmeetup" element={<NewMeetup />} />
         <Route path="/favotites" element={<Favorites />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/Error.js
@@ -0,0 +1,21 @@
+import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+const Error = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      navigate("/");
+    }, 5000);
+
+    return () => clearTimeout(timerId);
+  }, [navigate]);
+  return (
+    <div>
+      <p>Error! This page is not exist.</p>
+    </div>
+  );
+};
+
+export default Error;
